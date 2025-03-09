edc19072a5968c18827854da27f6d3c04cab1c16
Matheus Leal <matheuslealpa@gmail.com>
2025-03-09 15:38:07 -0300

    Initial commit

---
+++ src/main/java/com/app/SpringbootApp.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMemcachedLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMemcachedLabApplication.class, args);
+    }
+
+}
